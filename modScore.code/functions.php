f8156eb47f302f39bf31c862135bc1f2811c4b64
avijit sarkar <avisofficeone@gmail.com>
2025-08-18 15:13:38 +0530

    happy

--- modScore.code/functions.php
+++ modScore.code/functions.php
@@ -18,18 +18,6 @@ require_once  get_template_directory() . "/modules.fnc/module.enque.scripts.php"
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 if (! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
 	define('_S_VERSION', '1.0.0');
